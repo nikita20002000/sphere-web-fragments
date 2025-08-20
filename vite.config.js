d8519c22d5ec6564682ee5c91c2141b97c854f0e
nikitanovikov <Vertihost@yandex.com>
2025-08-20 20:05:19 +0300

    Fix and feat: fixed some scss files, added switch theme logic and optimized scss imports by resolving path in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/sphere-web-fragments/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
