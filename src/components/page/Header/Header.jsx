a781a04df4befde9f176603f8fc85f682d25b8a1
nikitanovikov <Vertihost@yandex.com>
2025-08-20 20:48:13 +0300

    Fix: fix theme changing logic and sidebar styles

--- src/components/page/Header/Header.jsx
+++ src/components/page/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Search from '../Search.jsx';
-import './Header.css';
+import './Header.scss';
 
 const Header = ({ searchQuery, onSearchChange }) => {
     return (
